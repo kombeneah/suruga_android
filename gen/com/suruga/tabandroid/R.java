68bea045e5586e640e4d8a790fa87e911b88b244
Eric Chang <changey@mit.edu>
2013-09-22 20:54:26 -0400

    settings

--- gen/com/suruga/tabandroid/R.java
+++ gen/com/suruga/tabandroid/R.java
@@ -13,6 +13,9 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int radio_colors=0x7f060000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int icon_photos_tab=0x7f020001;
@@ -20,15 +23,47 @@ public final class R {
         public static final int icon_videos_tab=0x7f020003;
         public static final int photos_gray=0x7f020004;
         public static final int photos_white=0x7f020005;
-        public static final int songs_gray=0x7f020006;
-        public static final int songs_white=0x7f020007;
-        public static final int videos_gray=0x7f020008;
-        public static final int videos_white=0x7f020009;
+        public static final int segment_button=0x7f020006;
+        public static final int segment_grey=0x7f020007;
+        public static final int segment_grey_focus=0x7f020008;
+        public static final int segment_grey_press=0x7f020009;
+        public static final int segment_radio_grey_left=0x7f02000a;
+        public static final int segment_radio_grey_left_focus=0x7f02000b;
+        public static final int segment_radio_grey_left_press=0x7f02000c;
+        public static final int segment_radio_grey_middle=0x7f02000d;
+        public static final int segment_radio_grey_middle_focus=0x7f02000e;
+        public static final int segment_radio_grey_middle_press=0x7f02000f;
+        public static final int segment_radio_grey_right=0x7f020010;
+        public static final int segment_radio_grey_right_focus=0x7f020011;
+        public static final int segment_radio_grey_right_press=0x7f020012;
+        public static final int segment_radio_left=0x7f020013;
+        public static final int segment_radio_middle=0x7f020014;
+        public static final int segment_radio_right=0x7f020015;
+        public static final int segment_radio_white_left=0x7f020016;
+        public static final int segment_radio_white_left_focus=0x7f020017;
+        public static final int segment_radio_white_left_press=0x7f020018;
+        public static final int segment_radio_white_middle=0x7f020019;
+        public static final int segment_radio_white_middle_focus=0x7f02001a;
+        public static final int segment_radio_white_middle_press=0x7f02001b;
+        public static final int segment_radio_white_right=0x7f02001c;
+        public static final int segment_radio_white_right_focus=0x7f02001d;
+        public static final int segment_radio_white_right_press=0x7f02001e;
+        public static final int segment_white=0x7f02001f;
+        public static final int segment_white_focus=0x7f020020;
+        public static final int songs_gray=0x7f020021;
+        public static final int songs_white=0x7f020022;
+        public static final int videos_gray=0x7f020023;
+        public static final int videos_white=0x7f020024;
     }
     public static final class id {
-        public static final int spinner1=0x7f060002;
-        public static final int textView1=0x7f060000;
-        public static final int textView2=0x7f060001;
+        public static final int TextView01=0x7f070006;
+        public static final int button_one=0x7f070004;
+        public static final int button_three=0x7f070005;
+        public static final int editText1=0x7f070007;
+        public static final int segment_text=0x7f070003;
+        public static final int spinner1=0x7f070002;
+        public static final int textView1=0x7f070000;
+        public static final int textView2=0x7f070001;
     }
     public static final class layout {
         public static final int analysis_layout=0x7f030000;
@@ -41,4 +76,25 @@ public final class R {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a CompoundButton.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #CompoundButton_android_button com.suruga.tabandroid:android_button}</code></td><td></td></tr>
+           </table>
+           @see #CompoundButton_android_button
+         */
+        public static final int[] CompoundButton = {
+            0x01010107
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.suruga.tabandroid.R.attr#android_button}
+          attribute's value can be found in the {@link #CompoundButton} array.
+          @attr name android:android_button
+        */
+        public static final int CompoundButton_android_button = 0;
+    };
 }
--- src/com/suruga/tabandroid/SettingsActivity.java
+++ src/com/suruga/tabandroid/SettingsActivity.java
@@ -2,21 +2,40 @@ package com.suruga.tabandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends Activity implements OnCheckedChangeListener {
 
 	private Spinner spinner1;
+	//The segment selection button
+	SegmentedRadioGroup segmentText;
+	Toast mToast;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settings_layout);
+		
+//		segmentText = (SegmentedRadioGroup) findViewById(R.id.segment_text);
+//        segmentText.setOnCheckedChangeListener(this);
 
 		addListenerOnSpinnerItemSelection();
+		segmentText = (SegmentedRadioGroup) findViewById(R.id.segment_text);
+        segmentText.setOnCheckedChangeListener(this);
+
+        mToast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
+	}
+	
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		if (group == segmentText) {
+			if (checkedId == R.id.button_one) {
+//				mToast.setText("One");
+//				mToast.show();
+			} else if (checkedId == R.id.button_three) {
+			}
+		} 
 	}
 
 	public void addListenerOnSpinnerItemSelection() {
